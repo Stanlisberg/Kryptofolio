f1b24a1f92c995dfda60d29a52123b99debb4aa5
Stanlisberg <nnabugo2010@gmail.com>
2022-10-23 15:09:02 -0700

    modified signin page

--- src/components/pages/SignIn.jsx
+++ src/components/pages/SignIn.jsx
@@ -3,16 +3,37 @@ import { FaUserTie } from 'react-icons/fa'
 import { useState, useRef } from 'react'
 
 function SignIn() {
-    const [removeIcon, setRemoveIcon] = useState(true);
-    const inputRef = useRef();
+    const [removeNameIcon, setRemoveNameIcon] = useState(true);
+    const [removeEmailIcon, setRemoveEmailIcon] = useState(true);
+    const [removePasswordIcon, setRemovePasswordIcon] = useState(true);
 
-    const clearInput = () => {
-       if(inputRef.current.value) {
-           setRemoveIcon(false)
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+    const clearNameInput = () => {
+       if(nameRef.current.value) {
+           setRemoveNameIcon(false)
         } else {
-            setRemoveIcon(true)
+            setRemoveNameIcon(true)
         }
     }
+
+    const clearEmailInput = () => {
+        if(emailRef.current.value) {
+            setRemoveEmailIcon(false)
+         } else {
+             setRemoveEmailIcon(true)
+         }
+     }
+
+     const clearPasswordInput = () => {
+        if(passwordRef.current.value) {
+            setRemovePasswordIcon(false)
+         } else {
+             setRemovePasswordIcon(true)
+         }
+     }
 
     return(
         <div className='max-w-[600px] mx-auto min-h-[500px] px-4 py-20 border'>
@@ -22,11 +43,20 @@ function SignIn() {
                  <label htmlFor="Name" className='text-gray-300'>Name</label>
                  <div className='my-2 w-full relative rounded-2xl shadow-xl'>
                    <input
+                    ref={nameRef}
+                    onFocus={() =>  setRemoveNameIcon(false) }
+                    onBlur={() =>  {
+                        setRemoveNameIcon(true)
+                        clearNameInput()
+                    }}
                     type='text'
                     className="bg-input w-full p-2 rounded-2xl"
                     placeholder='Name'
                    />
-                   <FaUserTie className='absolute left-2 top-3 text-gray-400' color='#64ffda'/>
+                    {removeNameIcon === true
+                    ? <FaUserTie className='absolute left-2 top-3 text-gray-400' color='#64ffda' />
+                    : ''
+                    }
                   </div>
                 </div>
 
@@ -34,11 +64,20 @@ function SignIn() {
                  <label htmlFor="Name" className='text-gray-300'>Email</label>
                   <div className='my-2 w-full relative rounded-2xl shadow-xl'>
                    <input
+                    ref={emailRef}
+                    onFocus={() =>  setRemoveEmailIcon(false) }
+                    onBlur={() =>  {
+                        setRemoveEmailIcon(true)
+                        clearEmailInput()
+                    }}
                     type='text'
                     className='bg-input w-full p-2 rounded-2xl'
                     placeholder='Email'
                     />
-                    <AiOutlineMail className='absolute left-2 top-3 text-gray-400' color='#64ffda' />
+                     {removeEmailIcon === true
+                    ? <AiOutlineMail className='absolute left-2 top-3 text-gray-400' color='#64ffda' />
+                    : ''
+                    }
                   </div>
                  </div>
 
@@ -47,25 +86,25 @@ function SignIn() {
                   <div className='my-2 w-full relative rounded-2xl shadow-xl'>
                     {/* <div className='max-w-[500px] border'> */}
                    <input
-                    ref={inputRef}
-                    onFocus={() =>  setRemoveIcon(false) }
+                    ref={passwordRef}
+                    onFocus={() =>  setRemovePasswordIcon(false) }
                     onBlur={() =>  {
-                        setRemoveIcon(true)
-                        clearInput()
+                        setRemovePasswordIcon(true)
+                        clearPasswordInput()
                     }}
                     type='text'
                     className="bg-input w-full p-2 rounded-2xl"
                     placeholder='Password'
                    />
                    {/* </div> */}
-                    {removeIcon === true
+                    {removePasswordIcon === true
                     ? <AiFillLock className='absolute left-2 top-3 text-gray-400' color='#64ffda' />
                     : ''
                     }
-                    
                   </div>
                  </div>
             </form>
+            
 
         </div>
     )
