00d6787b8bb39f443e86b24c694a3615205fde02
Stanlisberg <nnabugo2010@gmail.com>
2022-10-23 14:08:18 -0700

    Created sign-in page

--- src/components/pages/SignIn.jsx
+++ src/components/pages/SignIn.jsx
@@ -1,27 +1,70 @@
+import { AiOutlineMail, AiFillLock } from 'react-icons/ai'
+import { FaUser } from 'react-icons/fa'
+import { useState, useRef } from 'react'
+
 function SignIn() {
+    const [removeIcon, setRemoveIcon] = useState(true);
+    const inputRef = useRef();
+
+    const clearInput = () => {
+       if(inputRef.current.value) {
+           setRemoveIcon(false)
+        } else {
+            setRemoveIcon(true)
+        }
+    }
+
     return(
-        <div>
-            <div>Create account!</div>
-            <form>
-                <label htmlFor="Name">Name</label>
-                 <input
-                   type='text'
-                   className=""
-                   placeHolder='Name'
-                 />
-                 <label htmlFor="Name">Email</label>
-                 <input
-                   type='text'
-                   className=""
-                   placeHolder='Name'
-                 />
-                 <label htmlFor="Name">Password</label>
-                 <input
-                   type='text'
-                   className=""
-                   placeHolder='Name'
-                 />
-                
+        <div className='max-w-[900px] mx-auto min-h-[500px] px-4 py-20 mt-4 border'>
+            <h2 className='mt-3 font-bold text-3xl overflow-hidden'>Create Account!</h2>
+            <form className='mt-5'>
+                <div className='my4'>
+                 <label htmlFor="Name" className='text-gray-300'>Name</label>
+                 <div className='my-2 w-full relative rounded-2xl shadow-xl'>
+                   <input
+                    type='text'
+                    className="bg-input w-full p-2 rounded-2xl"
+                    placeholder='Name'
+                   />
+                   <FaUser className='absolute left-2 top-3 text-gray-400' color='#64ffda'/>
+                  </div>
+                </div>
+
+                <div>
+                 <label htmlFor="Name" className='text-gray-300'>Email</label>
+                  <div className='my-2 w-full relative rounded-2xl shadow-xl'>
+                   <input
+                    type='text'
+                    className='bg-input w-full p-2 rounded-2xl'
+                    placeholder='Email'
+                    />
+                    <AiOutlineMail className='absolute left-2 top-3 text-gray-400' color='#64ffda' />
+                  </div>
+                 </div>
+
+                 <div>
+                  <label htmlFor="Name" className='text-gray-300'>Password</label>
+                  <div className='my-2 w-full relative rounded-2xl shadow-xl'>
+                    {/* <div className='max-w-[500px] border'> */}
+                   <input
+                    ref={inputRef}
+                    onFocus={() =>  setRemoveIcon(false) }
+                    onBlur={() =>  {
+                        setRemoveIcon(true)
+                        clearInput()
+                    }}
+                    type='text'
+                    className="bg-input w-full p-2 rounded-2xl"
+                    placeholder='Password'
+                   />
+                   {/* </div> */}
+                    {removeIcon === true
+                    ? <AiFillLock className='absolute left-2 top-3 text-gray-400' color='#64ffda' />
+                    : ''
+                    }
+                    
+                  </div>
+                 </div>
             </form>
 
         </div>
